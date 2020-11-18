ac16b640ef339ae295a78672e0b4987fa02b181c
Tegutwo <alvintegutwo@gmail.com>
2020-11-19 01:09:03 +0300

    fix service file path

--- service.js
+++ service.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/service.js');
+      navigator.serviceWorker.register('service.js');
     });
   }
   //polyfill
